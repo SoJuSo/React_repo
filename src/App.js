2c7551abc6e4a749478a43b9c751130bba304006
King-Hot <asdfg7123@gmail.com>
2022-11-08 15:57:22 +0900

    text reset

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 //import './App.css';
 import React from 'react';
-import Counter from './Counter';
+import InputSample from './InputSample';
 
 function App() {
   const name = '김석주';
   
   return (
-    <Counter />
+    <InputSample />
   );
 }
 
