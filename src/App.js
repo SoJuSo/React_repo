229024cc201f592b919881dd96eafdc5103f9374
King-Hot <asdfg7123@gmail.com>
2022-11-08 12:56:00 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Hello from './Hello.js';
 
 function App() {
+  const name = '김석주';
+  const style = {
+    backgroundColor : 'yellow',
+    color : 'red',
+    fontSize: 60,
+    padding: '1rem'
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/*주석 with 중괄호*/}
+      <Hello name = {name} />
+      <div style={style}>{name}</div>
+      <div className = "box">안녕히계세요</div>
+    </>
   );
 }
 
