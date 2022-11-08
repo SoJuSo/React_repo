7eee495d1c6a17bf067b9988dcfec12bf4c2b58d
King-Hot <asdfg7123@gmail.com>
2022-11-08 15:40:27 +0900

    react pt

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import './App.css';
+//import './App.css';
 import React from 'react';
-import Hello from './Hello.js';
+import Counter from './Counter';
 
 function App() {
   const name = '김석주';
-  const style = {
-    backgroundColor : 'yellow',
-    color : 'red',
-    fontSize: 60,
-    padding: '1rem'
-  };
+  
   return (
-    <>
-      {/*주석 with 중괄호*/}
-      <Hello name = {name} />
-      <div style={style}>{name}</div>
-      <div className = "box">안녕히계세요</div>
-    </>
+    <Counter />
   );
 }
 
---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Hello({color, name, isSpecial}){
+    return(
+        <div style={{color}}>
+            { isSpecial && <b>*</b>}
+            안녕하세요 {name}입니다.
+        </div>
+    );
+}
+
+Hello.defaultProps = {
+    name: '이름없음'
+}
+
+export default Hello;
