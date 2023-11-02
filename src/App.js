cba974f9605a9188c7f7de8c53dd4c4b8a55e70b
SoJuSo <asdfg7123@gmail.com>
2023-11-03 00:23:59 +0900

    실습하던 내용 정리 및 commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 //import './App.css';
-import React from 'react';
-import InputSample from './InputSample';
+import React from "react";
+import InputSample from "./InputSample";
 
 function App() {
-  const name = '김석주';
-  
-  return (
-    <InputSample />
-  );
+  const name = "김석주";
+  console.log(name);
+  return <InputSample />;
 }
 
 export default App;
